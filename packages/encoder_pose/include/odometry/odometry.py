fdcbde4b4606de8f2632adf80c2f59a5e8708dc0
ShimaShahfar <shimashahfar@gmail.com>
2025-10-07 22:40:59 -0400

    odometry update

--- packages/encoder_pose/include/odometry/odometry.py
+++ packages/encoder_pose/include/odometry/odometry.py
@@ -14,7 +14,9 @@ def delta_phi(ticks: int, prev_ticks: int, resolution: int) -> float:
     """
 
     # TODO: these are random values, you have to implement your own solution in here
-    dphi = np.random.random()
+    alpha = 2 * np.pi / resolution
+    delta_ticks = ticks - prev_ticks
+    dphi = delta_ticks * alpha
     # ---
     return dphi
 
@@ -49,8 +51,13 @@ def estimate_pose(
     """
 
     # These are random values, replace with your own
-    x_curr = np.random.random()
-    y_curr = np.random.random()
-    theta_curr = np.random.random()
+    d_l = R * delta_phi_left
+    d_r = R * delta_phi_right
+    d_A = (d_l + d_r)/2
+    delta_theta = (d_r - d_l)/baseline
+
+    x_curr = x_prev + d_A * np.cos(theta_prev)
+    y_curr = y_prev + d_A * np.sin(theta_prev)
+    theta_curr = theta_prev + delta_theta
     # ---
     return x_curr, y_curr, theta_curr
